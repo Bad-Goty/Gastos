3f8b1e10cd2489b97f1d9dd33292dd1fccf9aa76
Goty <goty929@gmail.com>
2025-06-14 11:34:40 -0600

    Se agrego la navegacion y el SplashScreen

---
+++ app/src/main/java/com/example/gastos/presentation/views/ContenedorGastos.kt
@@ -0,0 +1,31 @@
+package com.example.gastos.presentation.views
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.example.gastos.presentation.components.MyScaffold
+
+@Composable
+fun ContenedorGastos(navController: NavController) {
+    BackHandler {
+        navController.navigate("home")
+        {
+            popUpTo(0) { inclusive = true } // Limpia el back stack
+            launchSingleTop = true
+        }
+    }
+
+    Box(Modifier.fillMaxSize()){
+        MyScaffold(
+            Title = "Gastos",
+            navController
+        ) {
+            Column { Text("Hola") }
+        }
+    }
+}
